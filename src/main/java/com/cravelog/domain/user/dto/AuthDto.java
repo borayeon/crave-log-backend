496d2173282b199d0037a07be2053705d4cd628e
Taeya <retsbe13@gmail.com>
2026-07-06 16:59:52 +0900

    수정: AuthDto

--- src/main/java/com/cravelog/domain/user/dto/AuthDto.java
+++ src/main/java/com/cravelog/domain/user/dto/AuthDto.java
@@ -1,33 +1,4 @@
-package com.cravelog.domain.user.package com.cravelog.domain.user.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-public class AuthDto {
-
-    @Getter
-    @NoArgsConstructor
-    public static class SignupRequest {
-        private String email;
-        private String password;
-        private String name;
-        private String handle; // 고유 ID
-    }
-
-    @Getter
-    @NoArgsConstructor
-    public static class LoginRequest {
-        private String email;
-        private String password;
-    }
-
-    @Getter
-    @AllArgsConstructor
-    public static class TokenResponse {
-        private String token;
-    }
-};
+package com.cravelog.domain.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
